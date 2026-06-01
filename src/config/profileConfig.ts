93399854037cbfffc05082231d088f2342c32fe4
1xifengdeyouxi <3091069683@qq.com>
2026-06-01 18:12:03 +0800

    feat：修改相应配置

--- src/config/profileConfig.ts
+++ src/config/profileConfig.ts
@@ -9,10 +9,10 @@ export const profileConfig: ProfileConfig = {
 	avatar: "assets/images/avatar.avif",
 
 	// 名字
-	name: "Firefly",
+	name: "小汐",
 
 	// 个人签名
-	bio: "Hello, I'm Firefly.",
+	bio: "Hello, 欢迎光临我的博客网站.",
 
 	// 链接配置
 	// 已经预装的图标集：fa7-brands，fa7-regular，fa7-solid，material-symbols，simple-icons
@@ -24,19 +24,13 @@ export const profileConfig: ProfileConfig = {
 		{
 			name: "qq",
 			icon: "fa7-brands:qq",
-			url: "https://qm.qq.com/q/ZGsFa8qX2G",
+			url: "",
 			showName: false,
 		},
 		{
 			name: "GitHub",
 			icon: "fa7-brands:github",
 			url: "https://github.com/1xifengdeyouxi",
-			showName: false,
-		},
-		{
-			name: "Gitee",
-			icon: "fa7-brands:gitee",
-			url: "https://gitee.com/xifeng_12138",
 			showName: false,
 		},
 		{
